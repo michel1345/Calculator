49252f3982765501aacaa426a30efcc79d83e3dc
CORPROOT\TAAELMI2 <Michel.Elias@Swisscom.com>
2018-12-14 08:04:33 +0100

    Output addition

--- src/main/java/ch/bbw/consolecalculator/App.java
+++ src/main/java/ch/bbw/consolecalculator/App.java
@@ -18,7 +18,7 @@ public class App
     	
     	valueA = 10;
     	valueB = 20;
-    	System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(10, 20));
+    	System.out.println(calculator.summe(valueA, valueB));
 	
     }
 }
